7a79e16f3145ceb5e2fd20364d4fb24e6ed850cf
Huan200799 <55988824+Huan200799@users.noreply.github.com>
2020-08-20 04:22:50 +0700

    Manager Categories

---
+++ resources/lang/en/message.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'addcate' => 'Add Categories',
+    'cate' => 'Category',
+    'catename' => 'Category Name',
+    'Dashboard' => 'Dashboard',
+    'List' => 'List',
+    'Category' => 'Category',
+    'You have 3 Notification' => 'You have 3 Notification',
+    'error' => 'Whoops! Something went wrong!',
+    'MyProfile' => 'My Profile',
+    'Logout' => 'Logout',
+    'edit' => 'ID không tồn tại',
+    'xoa' => 'ID không tồn tại',
+
+    'id' => 'ID',
+    'categoriesname' => 'Categories Name',
+    'parentid' => 'Parent ID',
+    'Edit' => 'Edit',
+    'Delete' => 'Delete',
+    'editcate' => 'Edit Categories',
+    'parent' => 'Parent ID',
+
+    'Product' => 'Product',
+    'AddCategories' => 'Add Categories',
+    'ListCategories' => 'List Categories',
+    'AddProduct' => 'Add Product',
+    'ListProduct' => 'List Product',
+
+    'AddProduct' => 'Add Product',
+    'Product' => 'Product',
+
+];
+
+?>
